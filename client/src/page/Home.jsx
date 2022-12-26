3d090c9066a8023e7f71d3f459f76a771d450ed2
Geric94 <eric.gausi@free.fr>
2022-12-26 13:50:13 +0100

    Higher Order Component

---
+++ client/src/page/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { PageHOC } from '../components';
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className='text-white text-xl'>Hello from Home</h1>
+    </div>
+  )
+};
+
+export default PageHOC(
+  Home,
+  <>Welcome to Avax Gods <br/> a Web3 NFT Card</>,
+  <>Connect your wallet to start playing <br/> the ultimate Web3 Battle Card Game</>
+);
